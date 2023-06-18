6b80b452dbcc9cf39a53adcda8cdaf80e408d83d
AdrijaDhar2003 <96368997+AdrijaDhar2003@users.noreply.github.com>
2023-06-18 19:32:02 +0530

    commit

--- models.py
+++ models.py
@@ -4,3 +4,25 @@ from flask import Flask
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  # Specify the database URI
 db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
+    expected_calories = db.Column(db.Integer, nullable=False)
+    entries = db.relationship('Entry', backref='user', lazy=True)
+
+class Entry(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    time = db.Column(db.Time, nullable=False)
+    text = db.Column(db.Text, nullable=False)
+    calories = db.Column(db.Integer)
+    is_below_expected = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+class Role(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(32), unique=True, nullable=False)
+
+# Define any additional relationships and fields as needed
