fa4c67364f72a2fba9af9607482cd3086ae0efb5
AdrijaDhar2003 <96368997+AdrijaDhar2003@users.noreply.github.com>
2023-06-18 19:40:17 +0530

    commit

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask
-from models import db
-
+from flask_sqlalchemy import SQLAlchemy
+from models import User, Entry, Role
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-db.init_app(app)
+db = SQLAlchemy(app)
+
+# Define your models and routes below
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run()
